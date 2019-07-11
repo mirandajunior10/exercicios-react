627692b8e9817d20a966f9443acbecf1e75c5bf5
mirandajunior10 <jorlanjr@gmail.com>
2019-07-11 14:01:52 -0300

    Aulas até Relação entre Componentes#1

---
+++ src/componentes/MultiElementos.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default props =>[
+        <h1>Parte 1</h1>,
+        <h2>Parte 2</h2>
+]
+//export default props =>
+//    <React.Fragment>
+//        <h1>Parte 1</h1>
+//        <h1>Parte 2</h1>
+//    </React.Fragment>
+
+//Solução 1. Mais usada!
+//export default props =>
+//    <div>
+//        <h1>Parte 1</h1>
+//        <h1>Parte 2</h1>
+//    </div>
+    
--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
+// import PrimeiroComponente from './componentes/PrimeiroComponente';
+//import CompA, { CompB as B} from './componentes/DoisComponentes';
+//import MultiElementos from './componentes/MultiElementos'
+import FamiliaSilva from './componentes/FamiliaSilva'
 
 const elemento = document.getElementById('root');
 
-ReactDOM.render(<h1>Olá React</h1>, elemento);
+ReactDOM.render(
+    <div>
+        <FamiliaSilva />
+
+        {/*<MultiElementos/>*/}
+        {/*<CompA valor = "Olá, eu sou A!"/>
+        <B valor = "B na área!"/>*/}
+        {/*<PrimeiroComponente valor="Bom dia"/>*/}
+    </div>
+, elemento);
