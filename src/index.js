9eb9169b12a7ed6998f06df1246e7b54eb74c732
mirandajunior10 <jorlanjr@gmail.com>
2019-07-12 17:00:11 -0300

    mais uma aula - interação direta entre componentes

--- src/index.js
+++ src/index.js
@@ -4,13 +4,17 @@ import React from 'react';
 //import CompA, { CompB as B} from './componentes/DoisComponentes';
 //import MultiElementos from './componentes/MultiElementos'
 import FamiliaSilva from './componentes/FamiliaSilva'
-
+import Familia from './componentes/Família';
+import Membro from './componentes/Membro';
 const elemento = document.getElementById('root');
 
 ReactDOM.render(
     <div>
-        <FamiliaSilva />
-
+        <Familia sobrenome="Pereira">
+            <Membro nome="André"/>
+            <Membro nome="Mariana"/>
+        </Familia>
+        <FamiliaSilva/>
         {/*<MultiElementos/>*/}
         {/*<CompA valor = "Olá, eu sou A!"/>
         <B valor = "B na área!"/>*/}
